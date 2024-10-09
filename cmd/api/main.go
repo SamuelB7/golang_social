7ef01a409a8e28bf83318c0d567364894c6a71ef
Samuel Belo <belo.samuel@gmail.com>
2024-10-09 10:04:04 -0300

    environment variables config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"log"
 	"social/internal/env"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	cfg := config{
 		address: env.GetString("ADDRESS", ":3000"),
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module social
 
 go 1.23.0
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
